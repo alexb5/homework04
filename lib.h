c4c687a6616e6323fc4e8450788f925d76ed9537
Александр Юричук <alexandr.urichuk@teko.biz>
2018-09-18 19:19:27 +0300

    Добавил документацию

--- lib.h
+++ lib.h
@@ -4,9 +4,25 @@
 #include <cstdint>
 
 namespace lib {
-
+/**
+ * @brief Return major version of project.
+ * 
+ * @return int 
+ */
 int version_major();
+
+/**
+ * @brief Return minor version of project.
+ * 
+ * @return int 
+ */
 int version_minor();
+
+/**
+ * @brief return patch version of project
+ * 
+ * @return int 
+ */
 int version_patch();
 
 } // namespace lib
