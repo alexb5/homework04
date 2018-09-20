569f3f9b517501ffc6262b26ee88dd606f8a0556
Александр Юричук <alexandr.urichuk@teko.biz>
2018-09-20 17:46:48 +0300

    Different realisations

--- lib.h
+++ lib.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#include <array>
-#include <cstdint>
+#include <algorithm>
+#include <ostream>
+#include <type_traits>
+#include <vector>
+#include <string>
+#include <list>
 
 namespace lib {
 /**
@@ -24,5 +28,97 @@ int version_minor();
  * @return int 
  */
 int version_patch();
+
+/**
+ * @brief Generic template of type trait is_container.
+ * 
+ * @tparam T 
+ */
+template<class T>
+struct is_container :  std::false_type {};
+
+/**
+ * @brief Specification of type trait is_container for std::vector.
+ * 
+ * @tparam T 
+ */
+template <typename... Args> 
+struct is_container<std::vector<Args...>>:std::true_type{};
+
+/**
+ * @brief Specification of type trait is_container for std::list.
+ * 
+ * @tparam T 
+ */
+template <typename... Args> 
+struct is_container<std::list<Args...>>:std::true_type{};
+
+template< class T >
+constexpr bool is_container_v = is_container<T>::value;
+
+template<class T>
+void print_ip(T ip, std::ostream& out)
+{
+    std::string ip_string{};
+    if constexpr(std::is_integral<T>::value) {
+        auto size = sizeof(T);
+        std::string ip_string{};
+        const auto begin = reinterpret_cast<uint8_t*>(&ip);
+        for (auto i = begin; i < begin + size; ++i) {
+            ip_string.append(std::to_string(*i));
+            ip_string.append(".");
+        }
+        ip_string.pop_back();
+    }
+    else constexpr(is_container_v<T>) {
+        for (auto& byte : ip) {
+            ip_string.append(std::to_string(byte));
+            ip_string.append(".");
+        }
+        ip_string.pop_back();    
+    }
+    else {
+        ip_string = ip;
+    }
+    out << ip_string << std::endl;
+}
+
+/**
+ * @brief  Specification of function for integral types.
+ * 
+ * @param ip Ip address in numeric format.
+ * @param out Stream for output.
+ */
+// template<class T, typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
+// void print_ip(T ip, std::ostream& out)
+// {
+//     auto size = sizeof(T);
+//     std::string ip_string{};
+//     const auto begin = reinterpret_cast<uint8_t*>(&ip);
+//     for (auto i = begin; i < begin + size; ++i) {
+//         ip_string.append(std::to_string(*i));
+//         ip_string.append(".");
+//     }
+//     ip_string.pop_back();
+//     out << ip_string << std::endl;
+// }
+
+/**
+ * @brief  Specification of function for container types.
+ * 
+ * @param ip Ip address in numeric format.
+ * @param out Stream for output.
+ */
+// template<class T, typename std::enable_if<is_container<T>::value>::type* = nullptr>
+// void print_ip(T ip, std::ostream& out)
+// {
+//     std::string ip_string{};
+//     for (auto& byte : ip) {
+//         ip_string.append(std::to_string(byte));
+//         ip_string.append(".");
+//     }
+//     ip_string.pop_back();
+//     out << ip_string << std::endl;
+// }
 
 } // namespace lib
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include "lib.h"
 
 #include <iostream>
-#include <map>
 
 int main(int argc, char const *argv[])
 {
+    lib::print_ip(char(-1), std::cout);
+    lib::print_ip(short(0), std::cout);
+    lib::print_ip(int(2130706433), std::cout);
+    lib::print_ip(long(8875824491850138409), std::cout);
+
+
+    // lib::print_ip(std::vector<int>({1,2,3}), std::cout);
+
+    lib::print_ip(std::string("1.2.3.4.5"), std::cout);
+
     return 0;
 }
